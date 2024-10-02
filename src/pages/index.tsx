7fa3b9c2274d5e1e390ed55eed422adc2d5c2098
serhatbek <serhat.bekk@gmail.com>
2024-10-02 16:12:58 +0300

    ✨ add project pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export { default as Compare } from "./Compare/Compare";
+export { default as MyList } from "./MyList/MyList";
+export { default as Pokemon } from "./Pokemon/Pokemon";
+export { default as Search } from "./Search/Search";
